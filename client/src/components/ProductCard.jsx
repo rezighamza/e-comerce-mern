974594d6f7c257a2e83809e77f652ed6d35ddf47
rezighamza <119344553+rezighamza@users.noreply.github.com>
2024-03-01 11:21:44 +0100

    Refactor ProductCard component to display product name, price, and image

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,29 +1,9 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-
-
 function ProductCard({ product }) {
-    const [image, setImage] = useState([])
-    useEffect(() => {
-        axios.get(`http://localhost:3300/product/image/${product._id}`)
-            .then(response => {
-            setImage(response.data)
-            })
-            .catch(error => {
-            console.error('There was an error!', error)
-            })
-        }, [])
-    
-
-
   return (
-    <div className="product-card">
-      <div className="product-card__image">
-      </div>
-      <div className="product-card__content">
-        <h2>{product.name}</h2>
-        <p>{product.price}</p>
-      </div>
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.price}</p>
+      <img src={`http://localhost:3300/product/image/${product._id}`} alt={product.name} />
     </div>
   )
 }
